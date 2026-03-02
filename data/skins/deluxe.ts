c855c7ef36ec543ca1c3403456d1784862e2ea1b
tushar-kant <tusharkantanayak713@gmail.com>
2026-03-02 11:06:48 +0530

    fix

--- data/skins/deluxe.ts
+++ data/skins/deluxe.ts
@@ -4,6 +4,15 @@ import { fmvpSkins } from "./deluxe/partone/fmvp";
 import { dragonSkins } from "./deluxe/partone/dragon";
 import { dinopalsSkins } from "./deluxe/partone/dinopals";
 import { createSkins } from "./deluxe/partone/create";
+import { covenantSkins } from "./deluxe/partone/covenant";
+import { championSkins } from "./deluxe/partone/champion";
+import { blazingSkins } from "./deluxe/partone/blazing";
+import { atomicSkins } from "./deluxe/partone/atomic";
+import { allstarSkins } from "./deluxe/partone/allstar";
+import { zodiacSkins } from "./deluxe/partone/zodiac";
+import { venomSkins } from "./deluxe/partone/venom";
+import { valentineSkins } from "./deluxe/partone/valentine";
+import { stunSkins } from "./deluxe/partone/stun";
 
 export const deluxeSkins: SkinItem[] = [
   ...lightbornSkins,
@@ -11,4 +20,13 @@ export const deluxeSkins: SkinItem[] = [
   ...dragonSkins,
   ...dinopalsSkins,
   ...createSkins,
+  ...covenantSkins,
+  ...championSkins,
+  ...blazingSkins,
+  ...atomicSkins,
+  ...allstarSkins,
+  ...zodiacSkins,
+  ...venomSkins,
+  ...valentineSkins,
+  ...stunSkins,
 ];
